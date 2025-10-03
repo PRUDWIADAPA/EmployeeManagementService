e79820a774074c4ce0670323b9da1dcfeb37195a
Prudwi Adapa <prudhviadapa431@gmail.com>
2025-10-03 16:50:04 -0400

    Prudwi ceo Commit

--- src/test/java/org/example/employeemanagementsystem/ControllerTest/EmployeeControllerTest.java
+++ src/test/java/org/example/employeemanagementsystem/ControllerTest/EmployeeControllerTest.java
@@ -17,6 +17,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -28,6 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @WebMvcTest(EmployeeManagementController.class)
+@ActiveProfiles("test")
 public class EmployeeControllerTest {
 
     @MockitoBean
